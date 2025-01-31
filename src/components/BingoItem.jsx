bc3bd519618b6642fac541a3a9bff203185d55ec
Jon-Michael Cubano <jon@jmcubano.com>
2025-01-31 14:58:54 -0500

    mark

--- src/components/BingoItem.jsx
+++ src/components/BingoItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import mark from "../assets/mark.webp";
 
 function BingoItem(props) {
   const { children, marked, onClick, freespace } = props;
@@ -7,10 +8,15 @@ function BingoItem(props) {
     <button
       onClick={onClick}
       disabled={freespace}
-      className={`relative overflow-hidden whitespace-break-spaces cursor-pointer text-[13px] sm:text-[14px] text-midnight text-center flex items-center justify-center aspect-[3/4] sm:aspect-square transition-all border-solid border-[0.5px] border-black disabled:pointer-events-none ${
-        marked ? "bg-yellow" : ""
-      } ${freespace ? "bg-opacity-0 font-semibold" : ""}`}
+      className={`relative overflow-hidden whitespace-break-spaces cursor-pointer text-[13px] sm:text-[14px] text-midnight text-center flex items-center justify-center aspect-[3/4] sm:aspect-square transition-all border-solid border-[0.5px] border-black disabled:pointer-events-none  ${
+        freespace ? "bg-opacity-0 font-semibold" : ""
+      }`}
     >
+      <img
+        className="absolute top-0 left-0 w-full h-full z-[0] opacity-0 object-cover"
+        src="/mark.webp"
+        style={marked && !freespace ? { opacity: "100%" } : {}}
+      />
       {children}
     </button>
   );
